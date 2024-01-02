01aedddd17b8eee3eb4489c95a2695c28885b072
afcanop <afcanop@gmail.com>
2024-01-02 16:30:37 -0500

    publicacion card ui

--- src/components/Publicaciones/index.tsx
+++ src/components/Publicaciones/index.tsx
@@ -4,10 +4,7 @@ import {RespuestaCeldaAsignar, RespuestaCeldaLLave} from 'interface/celda';
 import {
   Box,
   Button,
-  CloseIcon,
   FormControl,
-  HStack,
-  IconButton,
   Input,
   Row,
   Stack,
@@ -15,9 +12,23 @@ import {
   VStack,
   useToast,
   Alert as Alertnb,
+  AspectRatio,
+  Image,
+  Center,
+  Heading,
+  HStack,
+  Avatar,
 } from 'native-base';
-import React, {useState} from 'react';
-import {Alert, ScrollView, TouchableOpacity} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {
+  Alert,
+  Animated,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import {red} from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
 import StepIndicator from 'react-native-step-indicator';
 import {StepIndicatorStyles} from 'react-native-step-indicator/lib/typescript/src/types';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -58,6 +69,7 @@ export default function App() {
   const navigation = useNavigation();
 
   const [codigoConfirmacion, setCodigoConfirmacion] = useState<String>('');
+  const [megusta, setMegusta] = useState<boolean>(false);
   const actualizarPaginaActual = (numero: number) => {
     setPaginaActual(paginaActual + numero);
   };
@@ -120,6 +132,30 @@ export default function App() {
         description: respuestaApiCeldaLLave.errorMensaje,
       });
     }
+  };
+  const AnimatedIonicons = Animated.createAnimatedComponent(Ionicons);
+  const scaleValue = useRef(new Animated.Value(1)).current;
+
+  const handlePress = () => {
+    // Animación de pulso
+    Animated.sequence([
+      Animated.timing(scaleValue, {
+        toValue: 1.2,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+      Animated.spring(scaleValue, {
+        toValue: 1,
+        friction: 3,
+        tension: 40,
+        useNativeDriver: true,
+      }),
+    ]).start();
+    setMegusta(!megusta);
+  };
+
+  const animatedStyle = {
+    transform: [{scale: scaleValue}],
   };
 
   return (
@@ -239,6 +275,90 @@ export default function App() {
             </Stack>
           </Box>
         )}
+      </Box>
+      <Box
+        rounded="lg"
+        overflow="hidden"
+        borderColor="coolGray.200"
+        borderWidth="1"
+        _dark={{
+          borderColor: 'coolGray.600',
+          backgroundColor: 'gray.700',
+        }}
+        _light={{
+          backgroundColor: 'gray.50',
+        }}>
+        <Box>
+          <HStack
+            p={2}
+            justifyContent={'space-between'}
+            alignItems={'center'}
+            justifyItems={'center'}>
+            <HStack space={2} alignItems={'center'}>
+              <Avatar
+                bg="cyan.500"
+                alignSelf="center"
+                size="md"
+                source={{
+                  uri: 'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+                }}>
+                HS
+              </Avatar>
+              <Text>nombre</Text>
+            </HStack>
+
+            <TouchableOpacity>
+              <Ionicons
+                name={'ellipsis-vertical'}
+                size={32}
+                color={colores.primary}
+              />
+            </TouchableOpacity>
+          </HStack>
+          <AspectRatio w="100%" ratio={16 / 9}>
+            <Image
+              source={{
+                uri: 'https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg',
+              }}
+              alt="image"
+            />
+          </AspectRatio>
+        </Box>
+        <Stack p="2" space={3}>
+          <HStack space={2}>
+            <TouchableWithoutFeedback onPress={handlePress}>
+              <AnimatedIonicons
+                name={megusta ? 'heart-outline' : 'heart'}
+                size={25}
+                color={megusta ? colores.primary : colores.rojo['500']}
+                style={animatedStyle}
+              />
+            </TouchableWithoutFeedback>
+            <TouchableOpacity>
+              <Ionicons
+                name={'chatbox-outline'}
+                size={25}
+                color={colores.primary}
+              />
+            </TouchableOpacity>
+          </HStack>
+          <Text fontWeight="400">
+            Bengaluru (also called Bangalore) is the center of India's high-tech
+            industry. The city is also known for its parks and nightlife.
+          </Text>
+          <HStack alignItems="center" space={4} justifyContent="space-between">
+            <HStack alignItems="center">
+              <Text
+                color="coolGray.600"
+                _dark={{
+                  color: 'warmGray.200',
+                }}
+                fontWeight="400">
+                6 mins ago
+              </Text>
+            </HStack>
+          </HStack>
+        </Stack>
       </Box>
     </ScrollView>
   );
