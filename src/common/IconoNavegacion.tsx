67a64cf37c6c713928d088fd121706800ed16d54
andres <afcanop@gmail.com>
2024-01-24 11:05:48 -0500

    validar de celda para  icono navegacion

--- src/common/IconoNavegacion.tsx
+++ src/common/IconoNavegacion.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import {useNavigation, DrawerActions} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {TouchableOpacity} from 'react-native';
 import colores from 'assets/theme/colores';
+import {useSelector} from 'react-redux';
+import {RootState} from 'store/reducers';
 
-const IconoNavegacion = ({icon, ruta}) => {
+interface IconoNavegacionProps {
+  icon: string;
+  ruta: string;
+}
+
+const IconoNavegacion: React.FC<IconoNavegacionProps> = ({icon, ruta}) => {
   const navigation = useNavigation();
+  const celdaId = useSelector((state: RootState) => state.usuario.celdaId);
 
   return (
-    <TouchableOpacity onPress={() => navigation.navigate(ruta)}>
-      <Ionicons name={icon} size={30} color={colores.blanco} />
-    </TouchableOpacity>
+    <>
+      {celdaId ? (
+        <TouchableOpacity onPress={() => navigation.navigate(ruta)}>
+          <Ionicons name={icon} size={30} color={colores.blanco} />
+        </TouchableOpacity>
+      ) : null}
+    </>
   );
 };
 
